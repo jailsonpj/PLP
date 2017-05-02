9b07bd60ca0afbbbdaafbf85724a8886e72ea1f1
Jefferson A <jlda@users.noreply.github.com>
2017-05-02 18:34:46 -0400

    Update abcd.pl

--- lista_1/exer5/abcd.pl
+++ lista_1/exer5/abcd.pl
@@ -1,3 +1,15 @@
+% Criar um programa para receber diversos valores numéricos digitados pelo
+% usuário e gravar em um arquivo. A digitação dos valores deve ser feita por uma
+% rotina interativa, que solicita um valor numérico do usuário por vez, grava esse
+% valor no arquivo de saída, pede o próximo valor. A digitação será encerrada
+% quando o usuário digitar “fim”, que não deve ser gravado no arquivo.
+% A seguir, o programa deve fechar esse arquivo de saída, abri-lo como arquivo de
+% entrada, ler os valores gravados anteriormente no arquivo, um a um, calcular a
+% raiz quadrada de cada valor lido do arquivo e exibir o resultado no monitor.
+% Terminar com a mensagem “Fim de processamento”, exibida no monitor.
+
+
+
 % Lê valores de um arquivos (valores finalizados com '.') e 
 % mostra na tela (por enquanto imprimindo "end_of_file').
 recebe :-

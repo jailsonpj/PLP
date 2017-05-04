d36ad111e7b811bef9aa74bd345c53d1903fa515
jailsonpj <jailsoncolares@gmail.com>
2017-05-03 20:52:02 -0400

    puts

--- lista_1/exer1.pl
+++ lista_1/exer1.pl
@@ -25,8 +25,11 @@ gerou(X,Y):- pai(X,Y);mae(X,Y).
 filho(X,Y):- homem(Y),pai(X,Y);mae(X,Y).
 filha(X,Y):- mulher(Y),pai(X,Y);mae(X,Y).
 irmao(X,Y):- (pai(Z,X),pai(Z,Y));(mae(D,X),mae(D,Y)),X\=Y.
-tio(X,Y):- gerou(Z,Y),irmao(Z,X).
+tio(X,Y):- homem(X),gerou(Z,Y),irmao(Z,X).
+tia(X,Y):- mulher(X),gerou(Z,Y),irmao(Z,X).
+avo(X,Y):- homem(X), pai(Z,Y),pai(X,Z).
+ava(X,Y):- mulher(X),gerou(Z,Y),filho(Z,X).
 primo(X,Y):- pai(Z,Y),tio(Z,X).
 
-ancestral(X,Y):- pai(X,Y).
-ancestral(X,Y):- pai(X,Y),ancestral(Z,Y).
+%ancestral(X,Y):- pai(X,Y).
+%ancestral(X,Y):- pai(X,Y),ancestral(Z,Y).
